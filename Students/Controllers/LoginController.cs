b50f606282dbfd5cdb3b18333d5447cc5c6dd074
Rahman <rahmanmajeed783@gmail.com>
2023-09-25 20:43:31 +0500

    crud using js

---
+++ Students/Controllers/LoginController.cs
@@ -0,0 +1,80 @@
+﻿using Students.Context;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Students.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+
+        //db_testEntities dbobj = new db_testEntities();
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult LoginS(string username, string password)
+        {
+            // Check if credentials are valid
+            if (isValid(username, password))
+            {
+                // Create session and store user details
+                Session["User"] = username;
+
+                return Json(new { success = true });
+            }
+            else
+            {
+                return Json(new { success = false });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Logout()
+        {
+            // Destroy session
+            Session.Abandon();
+
+            return Json(new { success = true });
+        }
+
+        private bool isValid(string username, string password)
+        {
+            bool isValidUser = false;
+            using (SqlConnection con = new SqlConnection("Data Source=AR_RAHMAN;Initial Catalog=db_test;User ID=sa;Password=sql123@"))
+            {
+                // Create a SQL command to select the user with the given username and password
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Admin WHERE Email=@Username AND Password=@Password", con);
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                // Open the database connection
+                con.Open();
+
+                // Execute the SQL command and get the count of matching rows
+                int count = (int)cmd.ExecuteScalar();
+
+                // Check if there is a row that matches the given username and password
+                if (count > 0)
+                {
+                    isValidUser = true;
+                }
+
+                // Close the database connection
+                con.Close();
+            }
+
+            return isValidUser;
+        }
+    }
+}
---
+++ Students/Controllers/StudentController.cs
@@ -0,0 +1,83 @@
+﻿using Students.Context;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+
+namespace Students.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        db_testEntities dbObj = new db_testEntities();
+
+        // GET: Student
+
+        public ActionResult Student()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AddStudent(tbl_Student model)
+        {
+            tbl_Student obj = new tbl_Student();
+            obj.ID= model.ID;
+            obj.Name = model.Name;
+            obj.FName = model.FName;
+            obj.Email = model.Email;
+            obj.Mobile = model.Mobile;
+            obj.Description = model.Description;
+
+            if (model.ID == 0)
+            {
+                dbObj.tbl_Student.Add(obj);
+                dbObj.SaveChanges();
+            }
+            else
+            {
+                dbObj.Entry(obj).State = (System.Data.Entity.EntityState)EntityState.Modified;
+                dbObj.SaveChanges();
+
+            }
+
+            return Json(obj, JsonRequestBehavior.AllowGet);
+            
+        }
+
+
+
+        [WebMethod]
+        public JsonResult studentList()
+        {
+            var res = dbObj.tbl_Student.ToList();
+            return Json(res);
+        }
+
+
+
+
+        public JsonResult stdDel(int id)
+        {
+            var res = dbObj.tbl_Student.Find(id);
+            if (res != null)
+            {
+                dbObj.tbl_Student.Remove(res);
+                dbObj.SaveChanges();
+            }
+            var list = dbObj.tbl_Student.ToList();
+            return Json(list);
+        }
+
+
+        public ActionResult stdEdit(int id)
+        {
+            var res = dbObj.tbl_Student.Find(id);
+            return View(res);
+        }
+
+    }
+}
